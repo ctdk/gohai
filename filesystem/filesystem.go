45694748e41422c0fe0cb8cbdf9a8adfd7b6f329
ctdk <jbingham@gmail.com>
2014-10-15 22:47:05 -0700

    OS X filesystem stuff chugging along

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -16,11 +16,24 @@ func getFileSystemInfo() (interface{}, error) {
 		return nil, err
 	}
 	lines := strings.Split(string(out), "\n")
-	var fileSystemInfo []interface{} = make([]interface{}, len(lines)-2)
-	for i, line := range lines[1:] {
-		values := regexp.MustCompile("\\s+").Split(line, expectedLength)
+	fileSystemInfo := make(map[string]interface{}, len(lines)-2)
+	re := regexp.MustCompile(dfRegexp)
+	for _, line := range lines[1:] {
+		values := re.FindStringSubmatch(line)
 		if len(values) == expectedLength {
-			fileSystemInfo[i] = updatefileSystemInfo(values)
+			fileSystemInfo[values[1]] = updatefileSystemInfo(values)
+		}
+	}
+	out, err = exec.Command("mount").Output()
+	if err != nil {
+		return nil, err
+	}
+	lines = strings.Split(string(out), "\n")
+	mre := regexp.MustCompile(mountRegexp)
+	for _, line := range lines {
+		values := mre.FindStringSubmatch(line)
+		if len(values) == mountLength {
+			fileSystemInfo[values[1]] = setMountInfo(values, fileSystemInfo[values[1]].(map[string]interface{}))
 		}
 	}
 
--- filesystem/filesystem_darwin.go
+++ filesystem/filesystem_darwin.go
@@ -1,12 +1,41 @@
 package filesystem
 
-var dfOptions = []string{"-l", "-k"}
-var expectedLength = 9
+import (
+	"strconv"
+	"strings"
+)
 
-func updatefileSystemInfo(values []string) map[string]string {
-	return map[string]string{
-		"name":       values[0],
-		"kb_size":    values[1],
-		"mounted_on": values[8],
+const expectedLength = 10
+const mountLength = 5
+
+var dfOptions = []string{"-k"}
+var dfRegexp = `^(.+?)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+\%)\s+(\d+)\s+(\d+)\s+(\d+\%)\s+(.+)$`
+var mountRegexp = `^(.+?) on (.+?) \((.+?), (.+?)\)$`
+
+func updatefileSystemInfo(values []string) map[string]interface{} {
+	iused, _ := strconv.Atoi(values[6])
+	iavail, _ := strconv.Atoi(values[7])
+	totalInodes := iused + iavail
+	return map[string]interface{}{
+		"block_size": "1024",
+		"kb_size":    values[2],
+		"kb_used":    values[3],
+		"kb_available": values[4],
+		"percent_used": values[5],
+		"inodes_used": values[6],
+		"inodes_available": values[7],
+		"inodes_percent_used": values[8],
+		"total_inodes": strconv.Itoa(totalInodes),
+		"mount": values[9],
 	}
+}
+
+func setMountInfo(values []string, entry map[string]interface{}) map[string]interface{} {
+	if entry == nil {
+		entry = make(map[string]interface{})
+		entry["mount"] = values[2]
+	}
+	entry["fs_type"] = values[3]
+	entry["mount_options"] = strings.Split(values[4], ", ")
+	return entry
 }
